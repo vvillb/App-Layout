098440fa210ad65d08c3e6a84c7f774c00d3a3dc
Virginia Villalobos <virginiavt98@gmail.com>
2023-02-28 10:40:44 +0100

    upload vite app
    Vite React app with basic Layout

---
+++ AppLayout/ViteReactApp/vitereactapp/src/assets/pages/Breadcrumbs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Breadcrumbs() {
+  return (
+    <div>
+      <p>migas</p>
+    </div>
+  )
+}
+
+export default Breadcrumbs
---
+++ AppLayout/ViteReactApp/vitereactapp/src/assets/pages/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div>
+      <h5>Menú de navegación</h5>
+      <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+        <li>d</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu
