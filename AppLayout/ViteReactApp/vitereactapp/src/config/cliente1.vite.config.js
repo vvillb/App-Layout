0ff61574ff73f4c3933d7a43ceadedb909bfe352
Virginia Villalobos <virginiavt98@gmail.com>
2023-03-08 12:56:17 +0100

    babel
    configuración de babel con vite, en cada archivo clienteX.vite.config.js, las listas de presets y plugins están vacías

--- AppLayout/ViteReactApp/vitereactapp/src/config/cliente1.vite.config.js
+++ AppLayout/ViteReactApp/vitereactapp/src/config/cliente1.vite.config.js
@@ -2,7 +2,17 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins:[react()],
+    plugins:[react({
+    babel: {
+      presets: [],
+      // Your plugins run before any built-in transform (eg: Fast Refresh)
+      plugins: [],
+      // Use .babelrc files
+      babelrc: true,
+      // Use babel.config.js files
+      configFile: true,
+    }
+  })],
     resolve:{
         alias:{
             '@client-layout':`../../src/plugins/client1/Layout.jsx`,
