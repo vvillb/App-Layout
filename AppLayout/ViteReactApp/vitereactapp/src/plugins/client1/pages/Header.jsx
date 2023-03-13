89a460b6bb30c1687de8c5579294b01daaba49c3
Virginia Villalobos <virginiavt98@gmail.com>
2023-03-13 08:27:13 +0100

    i18n config (?)

--- AppLayout/ViteReactApp/vitereactapp/src/plugins/client1/pages/Header.jsx
+++ AppLayout/ViteReactApp/vitereactapp/src/plugins/client1/pages/Header.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      <h1>Header</h1>
+          <h1>Header cliente 1</h1>
+      
     </div>
   )
 }
 
 export default Header
+
+/*<ul>
+      <li>{t('save')}</li>
+        <li>{t('cancel')}</li>
+        <li>{t('delete')}</li>
+        <li>{t('error')}</li>
+      </ul>*/
