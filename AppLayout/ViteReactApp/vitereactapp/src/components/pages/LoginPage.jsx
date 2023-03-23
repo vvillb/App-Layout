afc1bc75f59c72864452e6ba86f93fa2dce9a982
Virginia Villalobos <virginiavt98@gmail.com>
2023-03-23 13:29:23 +0100

    Tailwindcss
    tailwind instalado y prueba en la login page

--- AppLayout/ViteReactApp/vitereactapp/src/components/pages/LoginPage.jsx
+++ AppLayout/ViteReactApp/vitereactapp/src/components/pages/LoginPage.jsx
@@ -6,6 +6,16 @@ function LoginPage() {
     <Layout>
       <div>
       <h1>Login</h1>
+      <div>
+      <h1 className="underline">
+        Hello world!
+      </h1>
+      <h2>
+        <div>Hola mundo 2</div>
+        <div className='ml-8 text-xs hover:text-base'>Hola mundo 3</div>
+      </h2>
+    </div>
+
     </div>
     </Layout>
     
