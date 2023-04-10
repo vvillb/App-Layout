ff5a6433cf546b20963246e4afcd7da369cc0f83
Virginia Villalobos <virginiavt98@gmail.com>
2023-04-10 12:46:04 +0200

    create post

--- AppLayout/ViteReactApp/web/src/models/CreatePost.tsx
+++ AppLayout/ViteReactApp/web/src/models/CreatePost.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { PostType } from './post.interface';
+import { Post } from '../api/api';
 
 const CreatePost = () => {
-  return (
-    <div>
-      create post
-    </div>
+
+    const [isError, setIsError] = useState<boolean>(false);
+    const [posts, setPosts] = useState<PostType[]>([]);
+
+
+    const handleClick = async () => {
+        const now = new Date();
+        const dateStr = now.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: '2-digit' });
+        const timeStr = now.toLocaleTimeString('en-GB', { hour12: false });
+
+        const newPost: PostType = {
+          title: "Nuevo título",
+          body: "Esta es una nueva tarjeta",
+          date: `${dateStr} ${timeStr}`,
+
+        };
+        try {
+          const response = await Post.createPost(newPost);
+          setPosts(prev => [response, ...prev]);
+        } catch (error) {
+          setIsError(true);
+        }
+      };
+    return (
+        <div className='axiosExample'>
+            <h1 className='axiosExample'>Añadir sugerencia</h1>
+            <button onClick={handleClick} className='axiosExample'>Haz click aquí para generar una nueva tarjeta</button>
+
+            <div className='gridAxios'>
+                {
+                    posts.map(post => (
+                        <div key={post.userId} className='axiosExample'>
+                            <p className='axiosExample'>Título: <span>{post.title}</span></p>
+                            <p className='axiosExample'> <span>{post.body}</span></p>
+                            <p className='axiosExample'>fecha:<span>{post.date}</span></p>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
   )
 }
 
--- AppLayout/ViteReactApp/web/src/models/DeletePost.tsx
+++ AppLayout/ViteReactApp/web/src/models/DeletePost.tsx
@@ -3,6 +3,7 @@ import './axiosExample.css'
 import React, { useEffect, useState } from 'react'
 import { PostType } from './post.interface';
 import {Post}  from '../api/api';
+import {Button as DevButton} from 'devextreme-react/Button';
 
 const DeletePost = () => {
     const [isError, setIsError] = useState<boolean>(false);
@@ -28,15 +29,16 @@ const DeletePost = () => {
   return (
     <div className='axiosExample'>
             <h1 className='axiosExample'>Eliminar queja</h1> <br />
-            <h2 className='axiosExample'>Haz click en una trajeta</h2>
+            
             <div className="gridAxios">
                 {
                     posts.map(post => (
-                        <div className="axiosExample" key={post.id} onClick={() => handleDelete(post.id)}>
+                        <div className="axiosExample" key={post.id} >
                             <p className='axiosExample'>ID: <span className='axiosExample'>{post.id}</span></p>
                             <p className='axiosExample'>Título: <span>{post.title}</span></p>
                             <p className='axiosExample'> <span>{post.body}</span></p>
                             <p className='axiosExample'>Autor: <span>{post.userId}</span></p>
+                            <DevButton onClick={() => handleDelete(post.id)}>Eliminar</DevButton>
                         </div>
                     ))
                 }
